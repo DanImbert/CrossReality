16cf1306eff97f99a4fea969244dc0f76381ea7d
Daniel Imbert <danielimbert90@gmail.com>
2022-02-15 14:18:57 +0200

    Assignment 2

---
+++ AR_Assignments_DanImbert/Assets/Scripts/RaycastExample.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+
+public class RaycastExample : MonoBehaviour
+{
+
+    [SerializeField] private ARRaycastManager aRRaycastManager;
+
+    [SerializeField] private Text uiText;
+    // Start is called before the first frame update  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CastRay();
+    }
+
+    public void CastRay()
+    {
+        Ray ray = new Ray(transform.localPosition, transform.forward);
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        if (aRRaycastManager.Raycast(ray, hits, UnityEngine.XR.ARSubsystems.TrackableType.AllTypes))
+        {
+
+            uiText.text = hits[0].sessionRelativeDistance.ToString();
+        }
+
+        else
+        {
+            uiText.text = "Nothing hit";
+        }
+    }
+}
